95b98b3037afc5b83db7fdad5ed2311da5805563
DaraKing <dario.knezovich@gmail.com>
2019-07-12 17:39:12 +0200

    added localization

---
+++ src/components/Partials/LocaleChanger.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="locale-changer">
+        <el-select v-model="$i18n.locale" size="mini">
+            <el-option
+                    v-for="(lang, i) in langs"
+                    :key="`Lang${i}`"
+                    :value="lang">
+                {{ lang }}
+            </el-option>
+        </el-select>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'LocaleChanger',
+  data () {
+    return { langs: ['hr', 'en'] }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#locale-changer {
+    width: 100px;
+    float: right;
+    display: flex;
+    align-items: center;
+}
+
+#locale-changer > div {
+    background: transparent;
+    color: #fff;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import './registerServiceWorker'
 import './plugins/element.js'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import i18n from './i18n'
 
 Vue.use(VueAxios, axios)
 
@@ -14,5 +15,9 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
+
+i18n.locale = 'en'
+i18n.locale = 'hr'
