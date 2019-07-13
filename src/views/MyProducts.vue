000772b96afdd14ad073b573b6ff69f222426790
DaraKing <dario.knezovich@gmail.com>
2019-07-13 20:18:16 +0200

    added user products and create new product

--- src/views/MyProducts.vue
+++ src/views/MyProducts.vue
@@ -1,18 +1,31 @@
 <template>
-    <div>
-        My products
-        <UserProducts></UserProducts>
+    <div id="products-container">
+        <el-tabs tab-position="left" style="height: 100%">
+            <el-tab-pane v-bind:label="$t('my-products')">
+                <UserProducts></UserProducts>
+            </el-tab-pane>
+            <el-tab-pane v-bind:label="$t('add-product')">
+                <AddProduct></AddProduct>
+            </el-tab-pane>
+        </el-tabs>
     </div>
 </template>
 
 <script>
 import UserProducts from '../components/Product/UserProducts'
+import AddProduct from '../components/Product/AddProduct'
 export default {
   name: 'MyProducts',
-  components: { UserProducts }
+  components: { AddProduct, UserProducts },
+  data () {
+    return {
+      tabPosition: 'left'
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@import "../scss/Products/products";
+@import "../scss/Products/user-products";
 </style>
