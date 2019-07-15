71b924198ba809c24c8acbc37ab927732e4d6757
DaraKing <dario.knezovich@gmail.com>
2019-07-15 17:16:09 +0200

    finished single auction

--- src/components/Auctions/Auction.vue
+++ src/components/Auctions/Auction.vue
@@ -4,9 +4,19 @@
             <Loading></Loading>
         </div>
         <div v-show="!loading">
+            <el-page-header @back="goBack" v-bind:content="auction.ProductName"></el-page-header>
             <el-row>
                 <el-col :span="4"><Timeline v-bind:data="auction"></Timeline></el-col>
-                <el-col :span="20"><Primary v-bind:data="auction"></Primary></el-col>
+                <el-col :span="20">
+                    <el-row>
+                        <el-col :span="24">
+                            <Primary v-bind:data="auction"></Primary>
+                        </el-col>
+                        <el-col :span="24">
+                            <Slider v-bind:data="auction.AuctionGallery"></Slider>
+                        </el-col>
+                    </el-row>
+                </el-col>
             </el-row>
         </div>
     </div>
@@ -18,9 +28,10 @@ import Loading from '../Common/Loading'
 import constants from '../../js/constants'
 import Timeline from './Info/Timeline'
 import Primary from './Info/Primary'
+import Slider from './Info/Slider'
 export default {
   name: 'Auction',
-  components: { Primary, Timeline, Loading },
+  components: { Slider, Primary, Timeline, Loading },
   props: ['auctionId'],
   data () {
     return {
@@ -60,11 +71,14 @@ export default {
         .catch(error => {
           console.log(error)
         })
+    },
+    goBack () {
+      console.log('go back')
     }
   }
 }
 </script>
 
 <style scoped>
-
+    @import "/src/scss/Auctions/_main.scss";
 </style>
--- src/components/Auctions/Info/Primary.vue
+++ src/components/Auctions/Info/Primary.vue
@@ -1,5 +1,34 @@
 <template>
-    <div>{{ data.ProductSort }}</div>
+    <div>
+        <el-row :gutter="20">
+            <el-col :span="8"><div>
+                <span class="auction-label">Product name</span>
+                <span class="auction-information">{{ data.ProductName }}</span>
+            </div></el-col>
+            <el-col :span="8"><div>
+                <span class="auction-label">Product sort</span>
+                <span class="auction-information">{{ data.ProductSort }}</span>
+            </div></el-col>
+            <el-col :span="8"><div>
+                <span class="auction-label">Current price</span>
+                <span class="auction-information">{{ data.CurrentPrice }}</span>
+            </div></el-col>
+        </el-row>
+        <el-row :gutter="20">
+            <el-col :span="8"><div>
+                <span class="auction-label">Start price</span>
+                <span class="auction-information">{{ data.StartPrice }}</span>
+            </div></el-col>
+            <el-col :span="8"><div>
+                <span class="auction-label">Number of offers</span>
+                <span class="auction-information">{{ data.NumberOfOffers }}</span>
+            </div></el-col>
+            <el-col :span="8"><div>
+                <span class="auction-label">Offers per hour</span>
+                <span class="auction-information">{{ data.OffersPerHour }}</span>
+            </div></el-col>
+        </el-row>
+    </div>
 </template>
 
 <script>
@@ -10,5 +39,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
