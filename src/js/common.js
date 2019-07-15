8b2e4d3126e4aa89e2286402605371ff3917bf5e
DaraKing <dario.knezovich@gmail.com>
2019-07-15 15:58:10 +0200

    finished login

--- src/js/common.js
+++ src/js/common.js
@@ -1,13 +1,40 @@
+import constants from 'constants'
+
 function validateEmail (email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   return re.test(String(email).toLowerCase())
+}
+
+function setTokens (data) {
+  localStorage.setItem('access_token', data.access_token)
+  localStorage.setItem('refresh_token', data.refresh_token)
 }
 
 function returnLoginBody (username, password) {
   return 'grant_type=password&username=' + username + '&password=' + password
 }
 
+function returnRefreshTokenBody () {
+  let refreshToken = localStorage.getItem('refresh_token')
+  return 'grant_type=refresh_token&refresh_token=' + refreshToken
+}
+
+function refreshToken () {
+  let body = returnRefreshTokenBody()
+  this.axios.get(constants.LOGIN_URL, body)
+    .then(response => {
+      if (response.status === constants.HTTP_STATUS_OK) {
+        setTokens(response.data)
+      }
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
+
 export default {
   validateEmail,
-  returnLoginBody
+  returnLoginBody,
+  setTokens,
+  refreshToken
 }
